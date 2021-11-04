619c264e0a3f3f597dc924541ed79ec6f720db0f
Alfonso Martínez <alfmar100391@icloud.com>
2021-11-04 13:51:07 -0600

    Import issue fixed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const tasksRoutes = require('./routes/tasks.routes');
-import cors from 'cors';
+const cors = require('cors');
 
 
 const app = express();
--- controllers/tasks.controller.js
+++ controllers/tasks.controller.js
@@ -2,28 +2,28 @@ const { getAllTasks, setTask, getTaskById, updateTask, deleteTask } = require('.
 
 const getTasks_controller = async (req, res) => {
   const tasks = await getAllTasks();
-  res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Origin", "*");
   res.json(tasks);
 }
 
 const getTask_controller = async (req, res) => {
   const { id } = req.params;
   const response = await getTaskById(id);
-  res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Origin", "*");
   res.json(response);
 }
 
 const deleteTask_controller = async (req, res) => {
   const { id } = req.params;
   const response = await deleteTask(id);
-  res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Origin", "*");
   res.json(response);
 }
 
 const setTask_controller = async (req, res) => {
   const objTask = req.query;
   const response = await setTask(objTask);
-  res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Origin", "*");
   res.json(response);
 }
 
@@ -31,7 +31,7 @@ const updateTask_controller = async (req, res) => {
   const { id } = req.params;
   const taskObj = req.query;
   const response = await updateTask(id, taskObj);
-  res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Origin", "*");
   res.json(response);
 }
 
