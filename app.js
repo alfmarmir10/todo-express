98a71bfb5921d9b7c3bf24ab236f265945565c92
Alfonso Martínez <alfmar100391@icloud.com>
2021-11-04 12:37:41 -0600

    Added CORS

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const tasksRoutes = require('./routes/tasks.routes');
+import cors from 'cors';
+
 
 const app = express();
+app.use(cors()); 
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
